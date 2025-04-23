8ff910d74a487dd2f129e6af8109becdd6267f61
Archie888 <miika.pirtola@gmail.com>
2025-04-24 00:00:48 +0300

    Create TokenType.cs

---
+++ MathExpressions/ExpressionParsing/TokenType.cs
@@ -0,0 +1,12 @@
+namespace MathExpressions
+{
+    public enum TokenType
+    {
+        Number,
+        Operator,
+        Identifier,
+        LeftParen,
+        RightParen,
+        Comma
+    }
+}
